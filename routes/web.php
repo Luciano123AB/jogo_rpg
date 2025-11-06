f80eacc19e1593cba912531c53782292360d8b2a
Luciano Eduardo <luciano123ab@gmail.com>
2025-11-06 00:40:36 -0300

    001(Home_Imagens)

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,7 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix("/")->group(function () {
-    Route::get("", [MainController::class, "home"])->name("home");
+    Route::controller(MainController::class)->group(function() {
+        Route::get("", "home")->name("home");
+    });
 });
