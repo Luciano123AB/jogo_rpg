8631cd3a33c5b7f927eaa8c25f45dba0aa7eebef
Luciano Eduardo <luciano123ab@gmail.com>
2025-11-06 23:23:00 -0300

    003(Creditos_Audios)

--- resources/views/layouts/audio.blade.php
+++ resources/views/layouts/audio.blade.php
@@ -2,6 +2,6 @@
     <source src="{{ asset("assets/audios/trilha_sonora_normal.mp3") }}" type="audio/mpeg">
 </audio>
 
-<a href="{{ route("musicaNormal") }}" class="cursor sombra botoes btn btn-dark border border-danger rounded-circle ms-3 px-3 py-2">
+<a href="{{ route("musicaNormal") }}" class="cursor sombra botoes btn btn-dark border border-danger rounded-circle ms-3 mb-2 px-3 py-2">
     <i class="cursor cor_fonte bi {{ session("musicaNormal") == "Desativado" ? "bi-volume-mute" : "bi-volume-up" }} fs-4"></i>
 </a>
