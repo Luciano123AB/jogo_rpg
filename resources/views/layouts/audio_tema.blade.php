62c1cce16f23ad5cc6616c08500e4b7226385849
Luciano Eduardo <luciano123ab@gmail.com>
2025-11-09 11:40:57 -0300

    005(Banco_Cadastrar)

--- resources/views/layouts/audio_tema.blade.php
+++ resources/views/layouts/audio_tema.blade.php
@@ -17,12 +17,12 @@
     <source src="{{ asset("assets/audios/$musica") }}" type="audio/mpeg">
 </audio>
 
-<div class="d-flex justify-content-between mb-2">
-    <a href="{{ route("musica") }}" class="cursor sombra botoes animate__animated animate__fadeInDown btn {{ session("tema") == "escuro" ? "btn-secondary" : "btn-dark" }} border {{ session("tema") == "escuro" ? "border-primary" : "border-danger" }} rounded-circle ms-3 px-3 py-2">
+<div class="animate__animated animate__fadeInDown d-flex justify-content-between mb-2">
+    <a href="{{ route("musica") }}" class="cursor sombra botoes btn {{ session("tema") == "escuro" ? "btn-secondary" : "btn-dark" }} border {{ session("tema") == "escuro" ? "border-primary" : "border-danger" }} rounded-circle ms-3 px-3 py-2">
         <i class="cursor {{ session("tema") == "escuro" ? "cor_fonte_escuro" : "cor_fonte_claro" }} bi {{ session("musica") == "Desativado" ? "bi-volume-mute-fill" : "bi-volume-up-fill" }} fs-4"></i>
     </a>
     
-    <a href="{{ route("tema") }}" class="cursor sombra botoes animate__animated animate__fadeInDown btn {{ session("tema") == "escuro" ? "btn-secondary" : "btn-dark" }} border {{ session("tema") == "escuro" ? "border-primary" : "border-danger" }} rounded-circle me-3 px-3 py-2">
+    <a href="{{ route("tema") }}" class="cursor sombra botoes btn {{ session("tema") == "escuro" ? "btn-secondary" : "btn-dark" }} border {{ session("tema") == "escuro" ? "border-primary" : "border-danger" }} rounded-circle me-3 px-3 py-2">
         <i class="cursor {{ session("tema") == "escuro" ? "cor_fonte_escuro" : "cor_fonte_claro" }} bi {{ session("tema") == "escuro" ? "bi-brightness-high-fill" : "bi-moon-stars-fill" }} fs-4"></i>
     </a>
 </div>
